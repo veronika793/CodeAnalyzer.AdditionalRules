2a313a22dd86b864014766b6fd2991346a596322
veronika <veronica.martinova@gmail.com>
2018-11-03 16:15:23 +0200

    Added line length restriction rule.

---
+++ CodeAnalyzer.AdditionalRules/CodeAnalyzer.AdditionalRules/CodeAnalyzer.AdditionalRules/Extentions.cs
@@ -0,0 +1,16 @@
+﻿namespace CodeAnalyzer.AdditionalRules
+{
+    using System.Collections.Immutable;
+    using System.Threading;
+
+    using Microsoft.CodeAnalysis;
+    using Microsoft.CodeAnalysis.Diagnostics;
+
+    static class Extentions
+    {
+        internal static ImmutableArray<AdditionalText> GetStyleCopSettings(
+            this AnalyzerOptions options,
+            CancellationToken cancellationToken)
+            => options.AdditionalFiles;
+    }
+}
